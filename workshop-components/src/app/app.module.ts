fd29aa3f205ce04cf6d968f429fa3f90739276c1
TeodorBoyadzhiev <79416864+TeodorBoyadzhiev@users.noreply.github.com>
2021-07-25 17:43:17 +0300

    Add files via upload

--- workshop-components/src/app/app.module.ts
+++ workshop-components/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentService } from './content.service';
 import { CoreModule } from './core/core.module';
-import { ThemeComponent } from './theme/theme.component';
+
+import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ThemeModule } from './themes/theme.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThemeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    UserModule,
+    AppRoutingModule,
+    ThemeModule
   ],
   providers: [
     ContentService
