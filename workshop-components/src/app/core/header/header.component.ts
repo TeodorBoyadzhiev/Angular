6ee1410b49100b90084688c0f615230b31dcecf8
TeodorBoyadzhiev <79416864+TeodorBoyadzhiev@users.noreply.github.com>
2021-07-27 17:51:57 +0300

    Add files via upload

--- workshop-components/src/app/core/header/header.component.ts
+++ workshop-components/src/app/core/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+  
+  get firstName(): string {
+    return this.userService.user?.firstName || '';
   }
 
+  constructor(
+    private userService: UserService,
+    private router:Router
+  ) { }
+
+  logout(): void {
+    this.userService.logout();
+    this.router.navigate(['/']);
+  }
 }
