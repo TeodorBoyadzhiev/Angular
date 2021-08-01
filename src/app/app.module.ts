d09e6822a36c5c53b15281760652f57942bc3883
TeodorBoyadzhiev <79416864+TeodorBoyadzhiev@users.noreply.github.com>
2021-08-01 23:56:30 +0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContentService } from './content.service';
+import { CoreModule } from './core/core.module';
+
+import { UserModule } from './user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ThemeModule } from './themes/theme.module';
+import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    UserModule,
+    SharedModule,
+    ThemeModule,
+    AppRoutingModule
+  ],
+  providers: [
+    ContentService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
